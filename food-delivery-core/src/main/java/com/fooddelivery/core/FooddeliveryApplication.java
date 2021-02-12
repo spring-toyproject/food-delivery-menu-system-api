3c58f9591506f663e0c7dcb1bc6ef6362941cf1c
kor-Chipmunk <rhj4862@gmail.com>
2021-02-12 18:12:00 +0900

    chore: rename api folder to core folder

--- food-delivery-core/src/main/java/com/fooddelivery/core/FooddeliveryApplication.java
+++ food-delivery-core/src/main/java/com/fooddelivery/core/FooddeliveryApplication.java
@@ -1,4 +1,4 @@
-package com.fooddelivery.api;
+package com.fooddelivery.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
